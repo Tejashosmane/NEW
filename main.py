591628a2546d882f55752edd5dc6e2c36f8d67c9
Tejashosmane <tejashosmane1357@gmail.com>
2024-10-10 00:04:01 +0530

    haa

--- main.py
+++ main.py
@@ -1,20 +1,2 @@
-class Person:
-    __name="anonymous"
-
-    def __init__(self) -> None:
-        print("init")
-        self.clsmtd()
-
-    def __hello(self):
-        print("hello")
-
-    def clsmtd(self):
-        print(self.__name)
-        self.__hello()
-
-class man(Person):
-    def __init__(self) -> None:
-        super().__init__()
-        
-
-m1=man()
+a,b=256,256
+print(a is b)
